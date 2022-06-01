840767cbeb73b53e7375dacb80c778770e4a1f2e
tsukanov1974 <tsukanov1974@yandex.ru>
2022-06-01 23:17:16 +0300

    Добавление кнопки закрытия программы

--- ChartEhamples.py
+++ ChartEhamples.py
@@ -2,10 +2,18 @@
 
 import tkinter as tk
 
+# Функция закрытия программы
+def do_close():
+      window.destroy()
+      
 # Создание главного окна
 window = tk.Tk()
 window.geometry("450x450")
 window.title("Программа построения графиков")
+
+# Добавление кнопки закрытия программы
+btnclose = tk.Button(window, text = "Закрыть", font = ('Helvetica', 10, 'bold'), command = do_close)
+btnclose.place(x = 330, y = 400, width = 90, height = 30)
 
 # Запуск цикла mainloop
 window.mainloop()
