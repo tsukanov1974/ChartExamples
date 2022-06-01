578cf516b75f27134ce2db6daa3b554f58ee05f8
tsukanov1974 <tsukanov1974@yandex.ru>
2022-06-01 22:47:53 +0300

    Добавление основы создания программы

---
+++ ChartEhamples.py
@@ -0,0 +1,11 @@
+# Программа построения графиков
+
+import tkinter as tk
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Программа построения графиков")
+
+# Запуск цикла mainloop
+window.mainloop()
