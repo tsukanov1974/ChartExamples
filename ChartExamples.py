0e015de3bf47c0862a55dd05b587de89bddbb2a3
tsukanov1974 <tsukanov1974@yandex.ru>
2022-06-05 22:40:32 +0300

    Добавление графиков

--- ChartExamples.py
+++ ChartExamples.py
@@ -6,6 +6,10 @@ import tkinter as tk
 def do_close():
       window.destroy()
       
+# Импорт внешних файлов
+import chart1
+import chart2
+
 # Создание главного окна
 window = tk.Tk()
 window.geometry("450x450")
@@ -14,6 +18,20 @@ window.title("Программа построения графиков")
 # Добавление метки заголовка
 lblTitle = tk.Label(text = "Примеры построения графиков", font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
 lblTitle.place(x = 55, y = 25)
+
+# Добавление кнопки и метки для графика 1
+btnChart1 = tk.Button(window, text = "График 1", font = ('Helvetica', 10, 'bold'), command = chart1.plot_chart)
+btnChart1.place(x = 40, y = 115, width = 90, height = 30)
+
+lblChart1 = tk.Label(text = "График синуса matplotlib")
+lblChart1.place(x = 170, y = 122)
+
+# Добавление кнопки и метки для графика 2
+btnChart2 = tk.Button(window, text = "График 2", font = ('Helvetica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2.place(x = 40, y = 165, width = 90, height = 30)
+
+lblChart2 = tk.Label(text = "Нормальное распределение")
+lblChart2.place(x = 170, y = 172)
 
 # Добавление кнопки закрытия программы
 btnclose = tk.Button(window, text = "Закрыть", font = ('Helvetica', 10, 'bold'), command = do_close)
