7fcbbf414b632a250c92304adfb2570b9bfd697d
tsukanov1974 <tsukanov1974@yandex.ru>
2022-06-07 23:21:19 +0300

    Добавление сдвоенной гистограммы Seaborn

--- ChartExamples.py
+++ ChartExamples.py
@@ -49,10 +49,10 @@ lblChart2 = tk.Label(text = "Гистограмма Seaborn")
 lblChart2.place(x = 170, y = 272)
 
 # Добавление кнопки и метки для графика 5
-btnChart2 = tk.Button(window, text = "График 5", font = ('Helvetica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2 = tk.Button(window, text = "График 5", font = ('Helvetica', 10, 'bold'), command = chart3.plot_chart2)
 btnChart2.place(x = 40, y = 315, width = 90, height = 30)
 
-lblChart2 = tk.Label(text = "Нормальное распределение")
+lblChart2 = tk.Label(text = "Сдвоенная гистограмма Seaborn")
 lblChart2.place(x = 170, y = 322)
 
 # Добавление кнопки и метки для графика 6
--- chart3.py
+++ chart3.py
@@ -18,3 +18,26 @@ def plot_chart():
                          )
     
       plt.show()
+      
+def plot_chart2():
+      sns.set()
+      
+      np.random.seed(33)
+      normal_data_a = np.random.normal(size = 500, loc = 100, scale = 6)
+      normal_data_b = np.random.normal(size = 500, loc = 107, scale = 5)
+      
+      df_normal_a = pd.DataFrame(data = normal_data_a,  columns = ['score']).assign(group = 'A')
+      df_normal_b = pd.DataFrame(data = normal_data_b,  columns = ['score']).assign(group = 'B')
+      
+      score_data = pd.concat([df_normal_a, df_normal_b], ignore_index = True)
+      
+      # print (score_data)
+      sns.histplot(data = score_data
+                         ,  x = 'score'
+                         , alpha = .7
+                         , hue = 'group'
+                         ,  bins = 50
+                         ,  kde = True
+                         )
+    
+      plt.show()
